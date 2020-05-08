3c57a57ecf1d87198f96758d5e8332b3c9db774e
Brig Lamoreaux <bklamoreaux@gmail.com>
2020-05-08 16:38:08 -0700

    Load from SQL

--- application.py
+++ application.py
@@ -1,19 +1,73 @@
 #!/usr/bin/python
 """Main flask application."""
 import os
-from flask import Flask, render_template, request
+import argparse
+import json
+from datetime import date, datetime, timezone
+from flask import Flask, render_template, Response
+import psycopg2
 
 app = Flask(__name__)
 
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        d_truncated = obj.replace(microsecond=0)
+        return d_truncated.replace(tzinfo=timezone.utc).timestamp()
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+@app.route("/api/predictions")
+def get_predictions():
+    """Return the list of predications as a json object"""
+    results = []
+
+    columns = ("pid", "name", "location", "latitude", "longitude", "type", "modtime")
+
+    try:
+        conn = psycopg2.connect(db_conn)
+        # create a cursor
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT pid, name, location, latitude, longitude, type, modtime FROM predictions"
+        )
+
+        for row in cur.fetchall():
+            results.append(dict(zip(columns, row)))
+
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print("Database connection closed.")
+
+    json_str = json.dumps(results, indent=2, sort_keys=True, default=json_serial)
+
+    return Response(json_str, mimetype="application/json")
+
+
 @app.route("/")
 def home():
-
-    map_api_key = os.environ.get("MAP_API_KEY")
+    """Home route for main page."""
     config = {"map_api_key": map_api_key}
     return render_template("pages/home.html", config=config)
 
 
 # Default port:
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Predict mission call location.", add_help=True,
+    )
+    parser.add_argument("--map_key", "-k", help="map api key")
+    parser.add_argument("--connection_string", "-s", help="db connection string")
+    args = parser.parse_args()
+
+    map_api_key = args.map_key or os.environ.get("MAP_API_KEY")
+    db_conn = args.connection_string or os.environ.get("DB_CONN_STRING")
+
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -3,46 +3,39 @@
 
     var map;
     var predictions = [];
-    // function loadMapScenario() 
 
     console.log("Module Loaded")
 
     function selectedSuggestion(suggestionResult) {
         console.log("Adding selections")
         map.entities.clear();
-        // map.entities.clear();
-        // map.setView({ bounds: suggestionResult.bestView });
         var pushpin = new Microsoft.Maps.Pushpin(suggestionResult.location);
         predictions.push(pushpin)
-        // map.entities.push(pushpin);
         map.entities.push(predictions)
-        // document.getElementById('printoutPanel').innerHTML =
-        //     'Suggestion: ' + suggestionResult.formattedSuggestion +
-        //         '<br> Lat: ' + suggestionResult.location.latitude +
-        //         '<br> Lon: ' + suggestionResult.location.longitude;
-        
-        // map.entities.push(pushpin)
-
         console.log(predictions.length)
-       
     }
 
-
-   
-    
-
-
     jQuery(window).on('load', function() {
+        
         console.log("window ready")
         var el
-      
-            
+
         el = jQuery("#myMap")[0]
         map = new Microsoft.Maps.Map(el, {
             center: new Microsoft.Maps.Location(0,0),
             zoom: 3,           
         });
 
+        jQuery.get("/api/predictions", function(data ){
+            // Add pin to map
+            for (var i = 0; i < data.length; i++) {
+                var loc = new Microsoft.Maps.Location(data[i].latitude, data[i].longitude)
+                var pushpin = new Microsoft.Maps.Pushpin(loc);
+                predictions.push(pushpin)
+            }
+            map.entities.push(predictions)
+
+        });
 
         // Add auto suggest to search
         Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', function () {
@@ -57,9 +50,7 @@
         jQuery("#searchBox").click(function(el){
             jQuery("#searchBox").val('')
         })
-
     
     });
     
-
 }(window.LMRX = window.LMRX || {}, jQuery));
